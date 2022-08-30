acd9f60405bc1b46ae9da6f97b210a5fdc291a02
Mary Dillon <mary.dillon09@gmail.com>
2022-08-30 16:11:36 -0700

    remove logging

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
-console.log('barf')
-console.log(process.env)
-
-mongoose.connect(  process.env.MONGODB_URI || 'mongodb://localhost:27017/book_search',
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/book_search',
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
